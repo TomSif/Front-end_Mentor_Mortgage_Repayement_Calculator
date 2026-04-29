600d844d38f2eeb1442117848758fa9038620cbb
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-29 17:01:09 +0200

    feat(hook): create useMortgageCalculator with monthly and total repayment calculation

---
+++ src/hooks/useMortgageCalculator.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Inputs } from "../types";
+
+interface Result {
+  monthlyPayment: number;
+  totalRepayment: number;
+}
+const useMortgageCalculator = () => {
+  const [result, setResult] = useState<Result | null>(null);
+
+  const calculate = (inputs: Inputs) => {
+    const loanAmount = Number(inputs.loanAmount);
+    const interestRate = Number(inputs.interestRate);
+    const duration = Number(inputs.duration);
+
+    const r = interestRate / 100 / 12;
+    const n = duration * 12;
+
+    const monthlyResult =
+      inputs.loanType === "repayment"
+        ? (loanAmount * (r * (1 + r) ** n)) / ((1 + r) ** n - 1)
+        : loanAmount * r;
+
+    const totalResult =
+      inputs.loanType === "repayment"
+        ? monthlyResult * n
+        : monthlyResult * n + loanAmount;
+
+    setResult({ monthlyPayment: monthlyResult, totalRepayment: totalResult });
+  };
+
+  return { result, calculate };
+};
+
+export default useMortgageCalculator;
