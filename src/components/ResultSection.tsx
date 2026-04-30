067acb5b9ed9380a91f68c31ce8ad21016c7584f
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-30 09:03:35 +0200

    feat(result): add ResultSection component

---
+++ src/components/ResultSection.tsx
@@ -0,0 +1,70 @@
+import { Result } from "../hooks/useMortgageCalculator";
+
+interface ResultSectionProps {
+  result: Result | null;
+}
+const ResultSection = ({ result }: ResultSectionProps) => {
+  const formatter = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  });
+  return (
+    <section className="px-6 py-8 bg-slate-900  md:rounded-b-lg w-full">
+      {result === null ? (
+        <article className="flex flex-col items-center gap-4 w-full ">
+          <div className="w-48  h-48">
+            <img
+              className="aspect-square object-cover"
+              src="/assets/images/illustration-empty.svg"
+              alt=""
+            />
+          </div>
+          <h2 className="text-white text-preset text-center">
+            Result shown here
+          </h2>
+          <p className="text-preset-4 text-slate-300">
+            Complete the form and click “calculate repayments” to see what your
+            monthly repayments would be.
+          </p>
+        </article>
+      ) : (
+        <article className="flex flex-col items-start gap-6 w-ful">
+          <div className="flex flex-col items-start gap-4">
+            <h2 className="text-preset-2 text-white">Your Result</h2>
+            <p className="text-preset-4 text-slate-300">
+              Your results are shown below based on the information you
+              provided. To adjust the results, edit the form and click
+              “calculate repayments” again.
+            </p>
+          </div>
+          <div className="bg-black/25 border-t-2 border-lime rounded-xl py-6 px-4 w-full">
+            <div className="flex flex-col gap-2 mb-4">
+              <h3 className="text-preset-4 text-slate-300">
+                Your monthly repayments
+              </h3>
+              <data
+                className="text-lime text-preset-1-mobile md:text-preset-1"
+                value={result.monthlyPayment.toFixed(2)}
+              >
+                {formatter.format(result.monthlyPayment)}
+              </data>
+            </div>
+            <div className="flex flex-col gap-2 mt-4">
+              <h4 className="text-preset-4 text-slate-300">
+                Total you'll repay over the term
+              </h4>
+              <data
+                className="text-white text-preset-2"
+                value={result.totalRepayment.toFixed(2)}
+              >
+                {formatter.format(result.totalRepayment)}
+              </data>
+            </div>
+          </div>
+        </article>
+      )}
+    </section>
+  );
+};
+
+export default ResultSection;
