ff34662b19868aefdcfbd1afecdd19f57c1e7e94
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-30 10:19:38 +0200

    feat(input): improve a11y and input filtering

--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -1,3 +1,4 @@
+import { useId } from "react";
 import { cn } from "../lib/cn";
 
 interface InputNumberProps {
@@ -17,6 +18,7 @@ const InputNumber = ({
   value,
   onChange,
 }: InputNumberProps) => {
+  const id = useId();
   return (
     <div className="flex flex-col gap-3 w-full">
       <label className="text-preset-4 text-slate-700" htmlFor={unity}>
@@ -41,14 +43,24 @@ const InputNumber = ({
           type="text"
           name={unity}
           id={unity}
-          min={1}
-          max={999999999}
+          maxLength={9}
           value={value}
-          onChange={(e) => onChange(e.target.value)}
+          aria-invalid={isError}
+          aria-describedby={id + "-error"}
+          onChange={(e) => {
+            let sanitizedValue = e.target.value.replace(/[^0-9.]/g, "");
+            const parts = sanitizedValue.split(".");
+            if (parts.length > 2) {
+              sanitizedValue = parts[0] + "." + parts.slice(1).join("");
+            }
+            onChange(sanitizedValue);
+          }}
         />
       </div>
       {isError && (
-        <p className="text-preset-5 text-red">This field is required</p>
+        <p id={id + "-error"} className="text-preset-5 text-red">
+          This field is required
+        </p>
       )}
     </div>
   );
